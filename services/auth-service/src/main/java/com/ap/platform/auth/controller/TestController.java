9bb8f5a1e326b154fc0ff9ac2144c4a5bb1316f3
adityap <adityap@cdac.in>
2026-03-11 15:21:02 +0530

    feat(auth-service): read propogation X-User header from API gateway in test endpoint

--- services/auth-service/src/main/java/com/ap/platform/auth/controller/TestController.java
+++ services/auth-service/src/main/java/com/ap/platform/auth/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.ap.platform.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
 	@GetMapping("/api/test")
-	public String test() {
-		return "Authenticated successfully";
+	public String test(@RequestHeader(value="X-User", required=false) String user) {
+		return "Authenticated user: "+user;
 	}
 }
